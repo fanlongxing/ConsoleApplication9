0af81195d6f22792208fc02a60c95b9615bf81eb
flx <fanlongxing@live.cn>
2017-09-17 15:37:09 +0800

    qqqqqqqqqqqq

--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -2,10 +2,46 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class CExample {
+private:
+	int a;
+public:
+	CExample(int b)
+	{
+		a = b;
+	}
+
+	CExample(const CExample& C)
+	{
+		a = C.a;
+	}
+	void Show()
+	{
+		cout << a << endl;
+	}
+};
 
 int main()
 {
-    return 0;
+	CExample A(100);
+	CExample B = A;
+	B.Show();
+	return 0;
 }
-
